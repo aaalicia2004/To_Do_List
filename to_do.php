7fa3d6d0fbd7e2488a5daafb174dafbd84e63e34
Alicia Resendez <AAALICIA2004@YAHOO.COM>
2014-05-21 10:11:39 -0500

    added Open File menu option

--- to_do.php
+++ to_do.php
@@ -1,43 +1,131 @@
 <?php
 
-// Create array to hold list of todo items
-$items = array();
+$items = array();   //This array holds the list of 
+                    //to-do items inserted by the user.
+function list_items($array)
+{
+    $list= ''; 
+    foreach ($array as $key => $value)
+    {
+        // $key++; This way modifies the key to the array.
+        $list .= "[" . ($key+1) . "] $value\n";  //this $key +1 only adds 1 to the key but does not modify it.
+    }
+    return $list;
+}
 
-// The loop!
-do {
-    // Iterate through list items
-    foreach ($items as $key => $item) {
-        // Display each item and a newline
-        $key++;
-        echo $key . ". " . $item . "\n";
+function get_input($upper = false)  //grabs the information from the user's input, capitalizes it, and stores it in $result
+{
+    $result = trim(fgets(STDIN));
+    
+    return $upper ? strtoupper($result) : $result;
+}
 
+function add_new_item($items) //This function was created to handle the new items that are entered into the array
+{
+    echo 'Enter item: ';    
+    $todo_item = get_input();
+    $location = "";
+
+    if (!empty($items)) 
+    {
+        echo "Where do you want to add the T0-Do item? (B)eginning or (E)nd?";
+        $location = get_input(true);
     }
 
-    // Show the menu options
-    echo '(N)ew item, (R)emove item, (Q)uit : ';
-
-    // Get the input from user
-    // Use trim() to remove whitespace and newlines
-    $input = trim(fgets(STDIN));
-
-    // Check for actionable input
-    if ($input == 'N'|| $input == 'n') {
-        // Ask for entry
-        echo 'Enter item: ';
-        // Add entry to list array
-        $items[] = trim(fgets(STDIN));
-    } elseif ($input == 'R' || $input == 'r') {
-        // Remove which item?
-        echo 'Enter item number to remove: ';
-        // Get array key
-        $key = trim(fgets(STDIN));
-        // Remove from array
-        unset($items[--$key]);
+    if ($location == 'B')
+    {
+       array_unshift($items, $todo_item);
     }
-// Exit when input is (Q)uit
-} while ($input != 'Q' && $input != 'q');
-// } while (!($input == 'Q' || $input == 'q'))
-// Say Goodbye!
+    elseif ($location == 'E')
+    {
+        array_push($items,$todo_item);
+    }
+    else
+    {
+        array_push($items, $todo_item);
+    }
+    
+    return $items;
+}
+
+function sort_menu($list)
+{
+    echo 'Sort (A)-Z, Sort (Z)-A, Sort (O)rder Entered, Sort (R)everse Order: '. PHP_EOL;
+    
+    switch(get_input(true)) {
+        case 'A':
+            asort($list);
+            break;
+        case 'Z':
+            arsort($list);
+            break;
+        case 'O':
+            ksort($list);
+            break;
+        case 'R':
+            krsort($list);
+            break;
+    }
+    return $list;
+}
+
+
+//THE BELOW FUNCTION IS MINE AND OMAR'S SOLUTION. THE ONE ABOVE IS BEN'S
+//     $input= get_input(true);
+
+//     if($input == 'A') {
+//         asort($list);
+
+//     } elseif ($input =='Z') {
+//         arsort($list);
+
+//     } elseif ($input == 'O') {
+//         ksort($list);
+
+//     } elseif ($input == 'R') {
+//         krsort($list);
+//     }
+//     return $list;
+// }
+
+
+do {       
+    echo list_items($items);
+
+    echo '(N)ew item, (O)pen file, (R)emove item, (S)ort items, (Q)uit : ';
+    
+    $input= get_input(true);
+
+    if ($input == 'N') 
+    {     
+        $items = add_new_item($items);
+    }
+    elseif ($input == 'O')
+    {
+        
+    }
+    elseif ($input == 'R') 
+    {
+        echo 'Enter item number to remove: ';   
+        $key = get_input();  
+        unset($items[$key-1]);// Remove from array
+        //$items=array_values($items); //array_values is a pre-built function from PHP.
+    }
+    elseif ($input == 'S')
+    {
+       $items = sort_menu($items);
+    }
+    elseif ($input == 'F')
+    {
+        array_shift($items);
+    }
+    elseif($input == 'L')
+    {
+        array_pop($items);
+    }
+} 
+while ($input != 'Q');   // Exit when input is (Q)uit
+    // } while (!($input == 'Q' || $input == 'q'))- This is the same thing as line 29 Q Statement
 echo "Goodbye!\n";
 
 // Exit with 0 errors
